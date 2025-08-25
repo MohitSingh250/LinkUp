201ebe788b1c6b3ae0bee67595621818be7b236a
Mohit Singh <mscrick01@gmail.com>
2025-08-25 18:52:47 +0530

    add user schema and update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const {userRouter} = require("./routes/user.js")
 
 app.use(express.json())
 
-const {userRouter} = require("./routes/user.js")
+app.use('/api/v1/users',userRouter)
 
 async function main(){
     
-    await mongoose.connect("mongodb+srv://mscrick01:achievemongodb@cluster0.ovfkuhl.mongodb.net/")
+    await mongoose.connect("mongodb+srv://mscrick01:achievemongodb@cluster0.ovfkuhl.mongodb.net/LinkUp")
     app.listen(3000)
 }
 main()
