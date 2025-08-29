add82c351ef9d8aa76a8dadc0b9191a48742fa59
Mohit Singh <mscrick01@gmail.com>
2025-08-29 14:12:04 +0530

    add routes and index.js

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const {userRouter} = require("./routes/users.routes.js")
+
+app.use(express.json())
+
+app.use('/api/v1/users',userRouter)
+
+async function main(){
+    
+    await mongoose.connect("mongodb+srv://mscrick01:achievemongodb@cluster0.ovfkuhl.mongodb.net/LinkUp")
+    app.listen(3000)
+}
+main()
